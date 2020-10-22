25cb7d8945cb1c94ed32eb776a070feef526b9cc
Marko Tolevski <marko@tolevski.com>
2020-10-22 16:13:08 +0200

    Added Header, Footer, Layout, _document Added API fetch initial props form hacker news

---
+++ components/layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+
+
+
+export default class AppLayout extends React.Component{
+
+	render() {
+        return (
+            <div>
+                <Header />
+                {this.props.children}
+                <Footer />
+            </div>
+        )
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,62 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import { useRouter } from 'next/router'
+import Layout from '../components/layout'
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <Layout>
+      <section>
+        {posts.map((post) => (
+          <article>
+          <div>Meta</div>
+            <Link href={post.url}>
+              <a>{post.title}</a>
+            </Link>
+          </article>
+        ))}
+     
+      </section>
+    </Layout>
   )
 }
+
+
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  let posts = await getData().then(data => {
+    return Promise.resolve(data)
+  })
+  console.log(posts);
+
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts
+    },
+  }
+}
+
+
+const fetchList = async () => {
+  const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+  const data = await res.json()
+  return Promise.resolve(data)
+}
+
+const getItemData = async item => {
+  const ress =  await fetch('https://hacker-news.firebaseio.com/v0/item/'+item+'.json')
+  const data = await ress.json()
+  return Promise.resolve(data)
+}
+
+const getData = async () => {
+  let list = await fetchList();
+  return Promise.all(list.slice(0, 5).map(item => getItemData(item)))
+}
+
+
